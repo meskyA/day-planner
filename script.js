f0d3565d9c5aa876be3a8c89049cf7fc83035ace
meskyA <mesky2015@gmail.com>
2022-07-19 21:39:50 -0700

    modiries Readme

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
+const store = window.localStorage;
 
-$("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
+const container = $(".container");
+
+const now = moment();
+
+
+// Set current date.
+$("#currentDay").text(now.format("dddd MMMM DD, YYYY"));
+
+var currentHour = moment().hour();
+      
+        
+ console.log(currentHour);
+
+var currentTime = moment();
+        currentTime = currentTime.startOf("hour");
