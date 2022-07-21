d5944f6bb899dcc7b5b3beec861a7a5ba43ed1a6
meskyA <mesky2015@gmail.com>
2022-07-20 22:08:18 -0700

    modified html to append classes to correct elements

--- script.js
+++ script.js
@@ -3,24 +3,49 @@ const store = window.localStorage;
 const container = $(".container");
 
 const now = moment();
-
+ 
 $("#currentDay").text(now.format("dddd MMMM DD, YYYY"));
 
 // color code time blocks for past, present and future
 
 function timeBlockColor() {
-        var hour = moment().hour();
+       
+        var timeNow = moment().hour();
 
         $(".time-block").each(function() {
-                var currentTime = parseInt($(this).attr("id"));
+                var blocktime = parseInt($(this).attr("id"));
 
-                if(currentTime  > hour){
-                        $(this).addClass("future").css("background-color", "green");
-                } else if(currentTime === hour) {
-                        $(this).addClass("present").css("background-color", "red");
+                if(blocktime > timeNow){
+                        $(this).addClass("future");
+                } else if(blocktime === timeNow) {
+                        $(this).addClass("present");
                 } else {
-                        $(this).addClass("past").css("background-color","gray");
+                        $(this).addClass("past");
                 }                       
                
         })
-};
+}
+
+// save event entries
+$(document).ready(function() {
+        $(".btn").on("click", function() {
+                var text = $(this).siblings(".form").val();
+                var time = $(this).parent(attr("id"));
+
+                localStorage.setItem(time, text);
+        })
+
+        
+})
+
+$("#9 .form").val(localStorage.getItem("9"));
+$("10 .form").val(localStorage.getItem("10"));
+$("11 .form").val(localStorage.getItem("11"));
+$("12 .form").val(localStorage.getItem("12"));
+$("13 .form").val(localStorage.getItem("13"));
+$("#14 .form").val(localStorage.getItem("14"));
+$("#15 .form").val(localStorage.getItem("15"));
+$("#16 .form").val(localStorage.getItem("16"));
+$("#17 .form").val(localStorage.getItem("17"));
+
+timeBlockColor();
